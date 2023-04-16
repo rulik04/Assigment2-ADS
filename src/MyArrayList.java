891c4fcf80461632d637c83bba061bac91ba8c9d
rulik04 <220625@astanait.edu.kz>
2023-04-16 21:31:54 +0600

    Methods: add, remove, clear, contains, indexOf, lastIndexOf

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -7,6 +7,7 @@ public class MyArrayList<T> implements MyList<T> {
         array = new Object[capacity];
     }
 
+    @Override
     public void add(T newItem) {
         if (size == capacity) {
             increaseBuffer();
@@ -23,10 +24,12 @@ public class MyArrayList<T> implements MyList<T> {
         array = newArray;
     }
 
+    @Override
     public T get(int index) {
         return (T) array[index];
     }
 
+    @Override
     public T remove(int index) {
 
         if (index < 0 || index >= size) {
@@ -40,7 +43,92 @@ public class MyArrayList<T> implements MyList<T> {
         return (T) array;
     }
 
+    @Override
     public int size() {
         return size;
+    }
+
+    @Override
+    public void add(T newItem, int index) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds for length " + size);
+        }
+        if (size + 1 >= capacity) {
+            increaseBuffer();
+        }
+        if (index > size) {
+            index = size;
+        }
+        for (int i = size; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = newItem;
+        size++;
+    }
+
+    @Override
+    public boolean remove(T item) {
+        if ((size == 0)) {
+            return false;
+        }
+        int i;
+        for (i = 0; i < size; i++) {
+            if (array[i] == null && item == null) {
+                break;
+            }
+            if ((array[i] != null) && (array[i].equals(item))) {
+                break;
+            }
+        }
+        if (i < size) {
+            for (int j = i; j < size - 1; j++) {
+                array[j] = array[j + 1];
+            }
+            size--;
+            return true;
+        }
+        return false;
+
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        int i;
+        for (i = 0; i < size; i++) {
+            if (array[i] == null && o == null) {
+                break;
+            }
+            if ((array[i] != null) && (array[i].equals(o))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        int i;
+        for (i = size - 1; i >= 0; i--) {
+            if (array[i] == null && o == null) {
+                break;
+            }
+            if ((array[i] != null) && (array[i].equals(o))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) >= 0;
     }
 }
--- src/MyList.java
+++ src/MyList.java
@@ -4,4 +4,10 @@ public interface MyList<T> {
     void add(T item);
     T remove(int index);
     T get(int index);
+    void add(T item, int index);
+    boolean remove (T item);
+    void clear();
+    int indexOf(Object o);
+    int lastIndexOf(Object o);
+    boolean contains(Object o);
 }
